8f6d1fb4f205b116b9f7127905c20daccf678628
tanmaymore06 <tanmaymore900@gmail.com>
2025-10-18 13:50:27 +0530

    WIP: 1) added created_at column without alembic by re-starting the server and, 2) the first letter of the base must be capitalized so, I corrected it

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from . import models
 
 # Start reading from the database.py then go to models.py and finally come back here.
 
-models.base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 # "models.base.metadata" goes to the the models.py specifically, the ORM Model 'Post(Base)' and gets all the metadata like, table structure, column specifications, etc.,.
 # The create_all() creates whatever info present in the ORM Model and targets to the connection to the database created in the engine 
 
@@ -31,52 +31,52 @@ def root():
     return {"message": "Welcome to my api, hehefff"}
 
 
-@app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    return {"data": posts}
+# @app.get("/posts")
+# def get_posts():
+#     cursor.execute("""SELECT * FROM posts""")
+#     posts = cursor.fetchall()
+#     return {"data": posts}
 
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
-    return {"new_post": new_post}
+# @app.post("/posts", status_code=status.HTTP_201_CREATED)
+# def create_posts(post: Post):
+#     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+#                    (post.title, post.content, post.published))
+#     new_post = cursor.fetchone()
+#     conn.commit()
+#     return {"new_post": new_post}
 
 
-@app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (int(id),))
-    p = cursor.fetchone()
-    if not p:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"post with id: {id} was not found")
-    return {"post_detail": p}
+# @app.get("/posts/{id}")
+# def get_post(id: int):
+#     cursor.execute("""SELECT * FROM posts WHERE id = %s""", (int(id),))
+#     p = cursor.fetchone()
+#     if not p:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+#                             detail=f"post with id: {id} was not found")
+#     return {"post_detail": p}
         
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT) 
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (int(id),))
-    delete_post = cursor.fetchone()
-    conn.commit()
-    if not delete_post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"post with id: {id} was not found")
-    return {"message": "post was successfully deleted", "data": delete_post}
+# @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT) 
+# def delete_post(id: int):
+#     cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (int(id),))
+#     delete_post = cursor.fetchone()
+#     conn.commit()
+#     if not delete_post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+#                             detail=f"post with id: {id} was not found")
+#     return {"message": "post was successfully deleted", "data": delete_post}
 
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-                   (post.title, post.content, post.published, int(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if not updated_post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"post with id: {id} was not found")
-    return {"updated_data": updated_post}
+# @app.put("/posts/{id}")
+# def update_post(id: int, post: Post):
+#     cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+#                    (post.title, post.content, post.published, int(id)))
+#     updated_post = cursor.fetchone()
+#     conn.commit()
+#     if not updated_post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+#                             detail=f"post with id: {id} was not found")
+#     return {"updated_data": updated_post}
--- app/models.py
+++ app/models.py
@@ -1,28 +1,18 @@
-# In this file, we'll:
-#           declare the Base class using the declarative_base(). The Base class provides functionalities to make a Python class as an ORM Model.
-# The ORM Model is actually a schema that defines the structure of a database table if and only if it doesn't exist already. If the table name already exists, it won't create a new table.
-# Each class variable represents a column in the table, and the class itself represents the table structure. 
 
-from sqlalchemy.orm import declarative_base,base
+
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, Boolean, String
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
-base = declarative_base() # Base class to create ORM Models. Any class that inherits from this base class will be an ORM Model
+Base = declarative_base() # The first letter of the Base should be capitalized
 
-class Post(base):
-  __tablename___ = 'posts'
+class Post(Base):
+  __tablename__ = 'posts'
 
   id = Column(Integer, primary_key=True, nullable=False)
   title = Column(String, nullable=False)
   content = Column(String, nullable=False)
-  published = Column(Boolean, default=True) # 'default=True' is not a right way to set a default value in the database. 
-  # We have to use 'server_default=True' to set a default value in the database which will be corrected in the next commit.
-  # The difference between 'default=True' and 'server_default=True' is that the former sets a default value at the application level, while the latter sets it at the database level.
-
-# The "model.base.metadata.create_all(dind=engine)" in the main.py file will look for a database table named 'posts' in the database and if it doesn't find one, it will create the table using the structure defined in this ORM Model.
-# Let's say we want to change a specification of a column or add a new column to the table, we can simply modify this ORM Model and run the application again but because the table name already exists in the database, it won't make any changes to the existing table.
-
-
-# The drawback of using ORM Models is that if we make any changes to the model, it won't reflect in the database automatically.
-# To make changes in the database schema, we have to use migration tools like Alembic.
-
-  
+  published = Column(Boolean, server_default=text('True')) 
+  created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')) 
+  # created_at uses the TIMESTAMP type from sqlalchemy and sets the default value to the current time using the text function
